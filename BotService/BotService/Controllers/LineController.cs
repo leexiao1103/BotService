cf92982b8b7ae6f2b123e84dcb47c7dd5b755e72
LeeXiao <gary19941103@gmail.com>
2019-07-20 14:41:06 +0800

    New project

---
+++ BotService/BotService/Controllers/LineController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BotService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LineController : ControllerBase
+    {
+        // GET api/line
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/line/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/line
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] object body)
+        {
+            
+            return Ok();
+        }
+
+        // PUT api/line/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/line/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
